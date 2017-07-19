4415f89072c45dc31521544ffa9db03358bf6d3b
gordosgithub <30273077+gordosgithub@users.noreply.github.com>
2017-07-19 18:11:14 -0500

    Add files via upload

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,7 @@
 function loadNews(){
-$( "#newsTxt1" ).load( "news/news_1.txt");
+$( "#newsTxt1" ).load( "news/news_1.html", function(response, status, jqxhr) {
+  alert( "Load Complete. Response:" + response + " || Status: " + status );
+});
 $('#newsTxt2').load('news/news_2.txt');
 $('#newsTxt3').load('news/news_3.txt');
 }
